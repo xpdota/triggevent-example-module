bd6be18bfb9a4ab0aeaa7b3a2cd9d7a611b18e8c
xp <smurfs@smurfs.verymad.net>
2022-11-12 10:14:33 -0800

    Added an event handler

--- src/main/java/com/bar/foo/MySampleTabOutOfTree.java
+++ src/main/java/com/bar/foo/MySampleTabOutOfTree.java
@@ -1,14 +1,20 @@
 package com.bar.foo;
 
+import gg.xp.reevent.events.InitEvent;
+import gg.xp.reevent.scan.HandleEvents;
 import gg.xp.reevent.scan.ScanMe;
 import gg.xp.xivsupport.gui.TitleBorderFullsizePanel;
 import gg.xp.xivsupport.gui.extra.PluginTab;
 
+import javax.naming.event.EventContext;
 import javax.swing.*;
 import java.awt.*;
 
 @ScanMe
 public class MySampleTabOutOfTree implements PluginTab {
+
+	private JLabel initLabel;
+
 	@Override
 	public String getTabName() {
 		return "My Sample Tab";
@@ -18,6 +24,15 @@ public class MySampleTabOutOfTree implements PluginTab {
 	public Component getTabContents() {
 		TitleBorderFullsizePanel panel = new TitleBorderFullsizePanel("My Sample Plugin Tab");
 		panel.add(new JLabel("It Works! (Out of tree version)"));
+		initLabel = new JLabel("No Init Event Received Yet...");
+		panel.add(initLabel);
 		return panel;
+	}
+
+	@HandleEvents
+	public void handleEvents(EventContext context, InitEvent init) {
+		if (init != null) {
+			initLabel.setText("Init Event Received!");
+		}
 	}
 }
